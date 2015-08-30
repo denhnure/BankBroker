bc24d3d580ddb2eb04cf7bdaa2606d071bc36f20
denhnure <denhnure@gmail.com>
2015-08-30 20:16:51 +0300

    Creating a model and a model mapping that correspond to a structure of a csv file

---
+++ BankBroker.Logic/Mappings/ExchangeRateMap.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using BankBroker.Logic.Models;
+using CsvHelper.Configuration;
+
+namespace BankBroker.Logic.Mappings
+{
+    public sealed class ExchangeRateMap : CsvClassMap<ExchangeRate>
+    {
+        public ExchangeRateMap()
+        {
+            Map(m => m.Country).Index(0);
+            Map(m => m.Unit).Index(1);
+            Map(m => m.Currency).Index(2);
+            Map(m => m.Change).Index(3);
+            Map(m => m.ChangeSign).Index(4);
+            Map(m => m.ForeignExchangeBuy).Index(5);
+            Map(m => m.ForeignExchangeSell).Index(6);
+            Map(m => m.ForeignExchangeCenter).Index(7);
+            Map(m => m.ForeignCurrencyBuy).Index(8);
+            Map(m => m.ForeignCurrencySell).Index(9);
+            Map(m => m.ForeignCurrencyCenter).Index(10);
+            Map(m => m.CNBExchangeRate).Index(11);
+        }
+    }
+}
---
+++ BankBroker.Logic/Models/ExchangeRate.cs
@@ -0,0 +1,29 @@
+﻿namespace BankBroker.Logic.Models
+{
+    public class ExchangeRate
+    {
+        public string Country { get; set; }
+
+        public int Unit { get; set; }
+
+        public string Currency { get; set; }
+
+        public float Change { get; set; }
+
+        public string ChangeSign { get; set; }
+
+        public float ForeignExchangeBuy { get; set; }
+
+        public float ForeignExchangeSell { get; set; }
+
+        public float ForeignExchangeCenter { get; set; }
+
+        public string ForeignCurrencyBuy { get; set; }
+
+        public string ForeignCurrencySell { get; set; }
+
+        public string ForeignCurrencyCenter { get; set; }
+
+        public string CNBExchangeRate { get; set; }
+    }
+}
